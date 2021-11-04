73d2d4b44afa1657a30322b7761557e8c12f9ede
richard wakeham <rwakeham27@outlook.com>
2021-11-04 21:33:49 +0000

    Added Comment model and table

---
+++ database/migrations/2021_11_04_212919_create_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string("content");
+            $table->dateTime("date_time_posted"); // YYYY-MM-DD HH:MM:SS
+            $table->unsignedInteger("upvotes");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
