db899abc1a834bcf1cc2a83c732a76cba446ad9d
richard wakeham <rwakeham27@outlook.com>
2021-11-04 21:28:52 +0000

    Added Post model and table

---
+++ database/migrations/2021_11_04_211728_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->string("content");
+            $table->unsignedInteger("upvotes");
+            $table->dateTime("date_time_posted"); // YYYY-MM-DD HH:MM:SS
+            $table->string("project_link")->nullable();
+            $table->string("image_link")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
