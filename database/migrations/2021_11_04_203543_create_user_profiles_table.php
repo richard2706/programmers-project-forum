50430085d69a44bd8fdebd3a6c3b8c6ff59a71e6
richard wakeham <rwakeham27@outlook.com>
2021-11-04 21:16:24 +0000

    Created UserProfile model and table

---
+++ database/migrations/2021_11_04_203543_create_user_profiles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string("username");
+            $table->enum("user_type", ["admin", "standard"]);
+            $table->date("registration_date");
+            $table->date("birthday")->nullable();
+            $table->string("description")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_profiles');
+    }
+}
