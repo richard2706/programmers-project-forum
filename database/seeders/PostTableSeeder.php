6008016d777d1d13366ec92752d7e57b9b3d4fe8
richard wakeham <rwakeham27@outlook.com>
2021-11-05 01:45:06 +0000

    Updated manually added posts dates

--- database/seeders/PostTableSeeder.php
+++ database/seeders/PostTableSeeder.php
@@ -19,7 +19,7 @@ class PostTableSeeder extends Seeder
         $post1->title = "Cool Fruits Project";
         $post1->content = "Look at this brilliant project I made!";
         $post1->upvotes = 3;
-        $post1->date_time_posted = "04-11-02 15:02:12";
+        $post1->date_time_posted = "2021-11-02 15:02:12";
         $coolKiwiUser = UserProfile::find(1);
         $coolKiwiUser->posts()->save($post1);
 
@@ -27,7 +27,7 @@ class PostTableSeeder extends Seeder
         $post2->title = "Best Veggies Project";
         $post2->content = "All the best vegetables!";
         $post2->upvotes = 1;
-        $post2->date_time_posted = "04-10-27 08:32:14";
+        $post2->date_time_posted = "2021-10-27 08:32:14";
         $strongAppleUser = UserProfile::find(2);
         $strongAppleUser->posts()->save($post2);
     }
