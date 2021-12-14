d047849ed43398adab5cf3d170164c246c1efa84
richard wakeham <rwakeham27@outlook.com>
2021-12-14 22:31:18 +0000

    Displayed listing of posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,7 +7,14 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    All posts will appear here
+                    @foreach ($posts as $post)
+                        <div>
+                            <b>{{ $post->title }}</b>
+                            <p>{{ $post->userProfile->username }}</p>
+                            <i>{{ $post->date_time_posted }}</i>
+                            <p>{{ $post->content }}</p>
+                        </div>
+                    @endforeach
                 </div>
             </div>
         </div>
