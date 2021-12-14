7e951381e0598bd0e668345f95e7a23f266391b2
richard wakeham <rwakeham27@outlook.com>
2021-12-14 23:33:40 +0000

    Styled link to post show page

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,13 +8,14 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     @foreach ($posts as $post)
-                        <div class="my-4 p-2 bg-gray-100 rounded hover:bg-gray-200">
-                            <b>{{ $post->title }}</b>
-                            <p>{{ $post->userProfile->username }}</p>
-                            <i>{{ $post->date_time_posted }}</i>
-                            <p>{{ substr($post->content, 0, 100) . ((strlen($post->content) > 100) ? '...' : '') }}</p>
-                            <a href="{{ route('posts.show', compact('post')) }}">See Post</a>
-                        </div>
+                        <a href="{{ route('posts.show', compact('post')) }}">
+                            <div class="my-4 p-2 bg-gray-100 rounded hover:bg-gray-200">
+                                <b>{{ $post->title }}</b>
+                                <p>{{ $post->userProfile->username }}</p>
+                                <i>{{ $post->date_time_posted }}</i>
+                                <p>{{ substr($post->content, 0, 100) . ((strlen($post->content) > 100) ? '...' : '') }}</p>
+                            </div>
+                        </a>
                     @endforeach
                 </div>
             </div>
