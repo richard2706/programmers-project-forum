5751aeb20f80d8088b8e28a85d5dc1df1b4e1859
richard wakeham <rwakeham27@outlook.com>
2021-12-14 18:05:18 +0000

    Added user profile table seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,9 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
         $this->call(UserTableSeeder::class);
-        // $this->call(UserProfileTableSeeder::class);
+        $this->call(UserProfileTableSeeder::class);
         // $this->call(PostTableSeeder::class);
         //comment
     }
---
+++ database/seeders/UserProfileTableSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\UserProfile;
+use Illuminate\Database\Seeder;
+
+class UserProfileTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $profile1 = new UserProfile;
+        $profile1->username = "coolKiwi";
+        $profile1->user_type = "standard";
+        $profile1->registration_date = "2021-11-01";
+        $profile1->birthday = "1999-02-10";
+        $profile1->description = "I love kiwis!";
+        User::find(1)->userProfile()->save($profile1);
+        
+        $profile2 = new UserProfile;
+        $profile2->username = "Strong Apple";
+        $profile2->user_type = "admin";
+        $profile2->registration_date = "2021-10-20";
+        User::find(2)->userProfile()->save($profile2);
+
+        // // Create a random number of posts for each user
+        // $numUsers = 15;
+        // for ($i=0; $i < $numUsers; $i++) { 
+        //     $numPosts = rand(0, 3);
+        //     UserProfile::factory()->count(1)->hasPosts($numPosts)->create();
+        // }
+    }
+}
