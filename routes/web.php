1f82e55b5a90b930e8810dab3cf2f0774abd5fa0
richard wakeham <rwakeham27@outlook.com>
2021-12-14 20:19:50 +0000

    Updated default home page

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 });
 
 Route::get('/home', function () {
-    return view('dashboard');
+    return view('posts.index');
 })->middleware(['auth'])->name('home');
 
 require __DIR__.'/auth.php';
