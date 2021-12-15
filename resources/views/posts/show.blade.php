5c8c2c92eb00c92b0a5454c96ce5ae5e6afd685f
richard wakeham <rwakeham27@outlook.com>
2021-12-15 22:50:30 +0000

    Updated post page formatting

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,15 +8,16 @@
             <div class="mb-2 p-2">
                 <a class="hover:underline" href="{{ route('home') }}">< Back to All Posts</a>
             </div>
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mb-4">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <div class="mb-6">
+                    <div>
                         <p class="text-sm">Posted by {{ $post->userProfile->username }}@if(Auth::user()->userProfile == $post->userProfile) (you)@endif, <i>{{ $post->date_time_posted }}</i></p>
                         @if($post->project_link)
                             <p>See project here: <a class="hover:underline" href="{{ $post->project_link }}">{{ $post->project_link }}</a></p>
                         @endif
                         <!-- Insert image here -->
-                        <p class="my-2">{{ $post->content }}</p>
+                        <p class="my-4">{{ $post->content }}</p>
                         @if (session('post_message'))
                             <p>{{ session('post_message') }}</p>
                         @endif
@@ -24,27 +25,33 @@
                             <a class="hover:underline" href="{{ route('posts.edit', compact('post')) }}">Edit Post</a>
                         @endif
                     </div>
+                </div>
+            </div>
 
-                    @if (session('comment_message'))
-                        <p>{{ session('comment_message') }}</p>
-                    @endif
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <div>
+                        @if (session('comment_message'))
+                            <p>{{ session('comment_message') }}</p>
+                        @endif
 
-                    <a class="hover:underline" href="{{ route('comments.create', compact('post')) }}">Add comment</a>
+                        <a class="hover:underline" href="{{ route('comments.create', compact('post')) }}">Add comment</a>
 
-                    @if($comments->count() == 0)
-                        <div class="mt-6 p-2 bg-gray-100 rounded">
-                            <p class="text-center">No comments</p>
-                        </div>
-                    @endif
+                        @if($comments->count() == 0)
+                            <div class="mt-6 p-2 bg-gray-100 rounded">
+                                <p class="text-center">No comments</p>
+                            </div>
+                        @endif
 
-                    @foreach ($comments as $comment)
-                        <div class="my-4 p-2 bg-gray-100 rounded" >
-                            <p class="text-sm">Posted by {{ $comment->userProfile->username }}, <i>{{ $comment->date_time_posted }}</i></p>
-                            <p class="mt-1">{{ $comment->content }}</p>
-                        </div>
-                    @endforeach
+                        @foreach ($comments as $comment)
+                            <div class="my-4 p-2 bg-gray-100 rounded" >
+                                <p class="text-sm">Posted by {{ $comment->userProfile->username }}, <i>{{ $comment->date_time_posted }}</i></p>
+                                <p class="mt-1">{{ $comment->content }}</p>
+                            </div>
+                        @endforeach
 
-                    {{ $comments->links() }}
+                        {{ $comments->links() }}
+                    </div>
                 </div>
             </div>
         </div>
