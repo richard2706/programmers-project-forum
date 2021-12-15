a9ad765251d156c6dc83319fb2775571f3b1013a
richard wakeham <rwakeham27@outlook.com>
2021-12-15 15:52:52 +0000

    Added successful post creation message

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -11,6 +11,9 @@
 
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+                    @if (session('message'))
+                        <p>{{ session('message') }}</p>
+                    @endif
 
                     @foreach ($posts as $post)
                         <a href="{{ route('posts.show', compact('post')) }}">
